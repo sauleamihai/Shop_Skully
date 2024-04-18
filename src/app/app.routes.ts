cfa6d774c01072a851ec8af9d207a18e3ce8f7cf
Saulea Mihai <saulea.s@yahoo.com>
2024-04-18 20:31:11 +0300

    added product page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
 import {BlackBoxComponent} from "./black-box/black-box.component";
+import {ProductPageComponent} from "./product-page/product-page.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
   { path: 'about', component: HeaderComponent},
   { path: 'contact', component: ContactComponent },
+  {path: 'product_page/:id',component:ProductPageComponent}
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 // Define an interface for the type of objects in the blackBoxes array
 interface BlackBox {
@@ -8,40 +9,65 @@ interface BlackBox {
   productName: string;
   price: string;
   isFavorite: boolean;
+  isVisible:boolean;
 }
 
 @Component({
 
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   blackBoxes: BlackBox[] = [
-    {id: 1, imageSrc: 'assets/img/shirt1.png', productName: 'Tricou Corpse', price: '19.99 $', isFavorite: false},
-    {id: 2, imageSrc: 'assets/img/shirt1.png', productName: 'Tricou Corpse', price: '19.99 $', isFavorite: false},
-    {id: 3, imageSrc: 'assets/img/shirt1.png', productName: 'Tricou Corpse', price: '19.99 $', isFavorite: false},
-    {id: 4, imageSrc: 'assets/img/shirt1.png', productName: 'Tricou Corpse', price: '19.99 $', isFavorite: false},
-    {id: 5, imageSrc: 'assets/img/shirt1.png', productName: 'Tricou Corpse', price: '19.99 $', isFavorite: false},
-    {id: 6, imageSrc: 'assets/img/shirt1.png', productName: 'Tricou Corpse', price: '19.99 $', isFavorite: false}
+    {id: 1, imageSrc: 'assets/img/shirt1.png', productName: 'Tricou Corpse', price: '19.99 $', isFavorite: false,isVisible:true},
+    {id: 2, imageSrc: 'assets/img/shirt1.png', productName: 'Tricou Corpse', price: '19.99 $', isFavorite: false,isVisible:true},
+    {id: 3, imageSrc: 'assets/img/pantaloni_1.png', productName: 'Pantaloni Corpse', price: '29.99 $', isFavorite: false,isVisible:true},
+    {id: 4, imageSrc: 'assets/img/shirt_2.png', productName: 'Tricou Alb cu Imprimeu', price: '29.99 $', isFavorite: false,isVisible:true},
+    {id: 5, imageSrc: 'assets/img/shirt1.png', productName: 'Tricou Corpse', price: '19.99 $', isFavorite: false,isVisible:true},
+    {id: 6, imageSrc: 'assets/img/poste_shirt.png', productName: 'Tricou Post Malone', price: '16.99 $', isFavorite: false,isVisible:true}
   ];
 
   favourites: BlackBox[] = [];
+  favouritePressed: boolean = false;
+
 
   toggleFavorite(id: number) {
     console.log("Toggle favorite method called for id:", id);
     const box = this.blackBoxes.find(b => b.id === id);
     if (box) {
       box.isFavorite = !box.isFavorite;
-      if (box.isFavorite)
+      if (box.isFavorite) {
         this.favourites.push(box);
-    } else {
-      const index = this.favourites.findIndex(f => f.id === id)
-      if (index !== -1) {
-        this.favourites.splice(index, 1);
+      } else {
+        const index = this.favourites.findIndex(f => f.id === id);
+        if (index !== -1) {
+          this.favourites.splice(index, 1);
+        }
       }
     }
   }
+
+  showFavorites() {
+    if (this.favourites.length === 0 || this.favouritePressed) {
+      // Show all boxes
+      this.blackBoxes.forEach(box => {
+        box.isVisible = true;
+      });
+      this.favouritePressed = false; // Reset the flag
+    } else {
+      // Show only favorites
+      this.blackBoxes.forEach(box => {
+        box.isVisible = box.isFavorite;
+      });
+      this.favouritePressed = true; // Set the flag
+    }
+  }
+
+  protected readonly RouterLink = RouterLink;
+  protected readonly RouterLinkActive = RouterLinkActive;
 }
+
+
