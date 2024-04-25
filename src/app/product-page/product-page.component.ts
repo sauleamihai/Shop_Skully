7ade259a33ae325ba170c805a2631ad9b1f564b2
Saulea Mihai <saulea.s@yahoo.com>
2024-04-25 15:54:18 +0300

    product page styling

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-product-page',
-  standalone: true,
-  imports: [],
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.css'
+  standalone: true,
+  styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent {
+  constructor(private renderer: Renderer2) {}
 
+  toggleAnimation() {
+    const images = document.querySelectorAll('.pictute_buttom img');
+    images.forEach(img => {
+      this.renderer.addClass(img, 'animated');
+    });
+  }
+
+  changeColor(target: EventTarget | null) {
+    if (target instanceof HTMLButtonElement) {
+      const button = target as HTMLButtonElement; // Type assertion
+      if (button.classList.contains('active')) {
+        this.renderer.removeClass(button, 'active');
+        button.style.backgroundColor = '#f0f0f0';
+        button.style.color = '#333';
+      } else {
+        this.renderer.addClass(button, 'active');
+        button.style.backgroundColor = '#7FBB29';
+        button.style.color = '#fff';
+      }
+    }
+}
 }
